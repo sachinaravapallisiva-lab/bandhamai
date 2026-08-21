6ac660c2b42a5531c08bd9a409a86b592cbf7718
sachinaravapallisiva-lab <sachin.aravapalli.siva@gmail.com>
2026-08-21 17:02:58 -0500

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Dashboard() {
+  return (
+    <div className="bg-blue-600 text-white p-4">
+      <h1 className="text-2xl font-bold">Bandhamai</h1>
+    </div>
+  );
+}
