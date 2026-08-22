a6abd5e84376c19f73acd9b777edac1c573cae7b
sachinaravapallisiva-lab <sachin.aravapalli.siva@gmail.com>
2026-08-22 00:08:09 -0500

    Create keyterms.ts

---
+++ lib/keyterms.ts
@@ -0,0 +1,19 @@
+export const KEYTERMS = [
+  // Cities & towns
+  "Hyderabad", "Secunderabad", "Vijayawada", "Guntur", "Warangal",
+  "Karimnagar", "Nizamabad", "Rajahmundry", "Kakinada", "Tirupati",
+  "Nellore", "Visakhapatnam", "Vizag", "Kurnool", "Anantapur",
+  "Khammam", "Ongole", "Eluru", "Bhimavaram", "Machilipatnam",
+  // Communities
+  "Kamma", "Reddy", "Kapu", "Naidu", "Velama", "Vysya",
+  "Mudiraj", "Gouda", "Balija", "Padmashali", "Yadav",
+  // Common names
+  "Lakshmi", "Srinivas", "Venkatesh", "Padma", "Anitha",
+  "Swathi", "Ramesh", "Sridhar", "Pravalika", "Bhavani",
+  "Sailaja", "Harika", "Chaitanya", "Manoj", "Divya",
+  "Sruthi", "Ananya", "Keerthi", "Rajesh", "Naveen",
+  "Sneha", "Deepthi", "Vamsi", "Kalyani", "Rohith",
+  // Matrimony vocabulary
+  "gotra", "rashi", "nakshatra", "kundli", "biodata",
+  "muhurat", "jathakam", "pelli", "sambandham",
+];
